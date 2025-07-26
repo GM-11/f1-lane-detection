643527d2a844089f56ece4a7a8f9bac00d36da99
GM-11 <mathuyrgopal@gmail.com>
2025-07-27 00:15:44 +0530

    changed windows size

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 from detect_lanes import detect_lanes_and_draw_lines
+from box_around_car import draw_box_around_car
 
 video = cv2.VideoCapture("./data/converted_spa_onboard.mp4")
 
@@ -13,8 +14,11 @@ else:
         if not ret:
             break
         frame_with_lines = detect_lanes_and_draw_lines(frame)
+        frame_with_box = draw_box_around_car(frame_with_lines)
 
-        cv2.imshow("Lane Detection", frame_with_lines)
+        cv2.namedWindow("Lane Detection", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("Lane Detection", 1000, 750)
+        cv2.imshow("Lane Detection", frame_with_box)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
     video.release()
